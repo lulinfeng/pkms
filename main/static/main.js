01cdc590f4d2f985ebf2da5e02294b698c5e9732
lulinfeng <811191000@qq.com>
2017-11-20 21:16:40 +0800

    menu use dark themes

--- main/static/main.js
+++ main/static/main.js
@@ -1,32 +1,7 @@
 $(function () {
 	page = {
-		editor: ace.edit("editor")
-	};
-
-	var init = function (data) {
-		page.editor.setTheme("ace/theme/twilight");
-		page.editor.session.setMode("ace/mode/rst");
-		page.editor.setKeyboardHandler("ace/keyboard/vim");
-		page.editor.commands.addCommand({
-			name: 'save',
-			bindKey: {win: "Ctrl-S", "mac": "Cmd-S"},
-			exec: save
-		});
-		page.editor.commands.addCommand({
-			name: 'quit',
-			exec: quit
-		});
-		ace.config.loadModule("ace/keyboard/vim", function(m) {
-			var VimApi = require("ace/keyboard/vim").CodeMirror.Vim;
-			VimApi.defineEx("write", "w", function(cm, input) {
-				cm.ace.execCommand("save");
-			});
-			VimApi.defineEx("quit", "q", function(cm, input) {
-				cm.ace.execCommand("quit");
-			});
-		});
-
-		$('#menu').jstree({
+		editor: ace.edit("editor"),
+		menuElement: $('#menu').jstree({
 			'core': {
 				'data': {
 					'url': '/menu/',
@@ -34,13 +9,15 @@ $(function () {
 				},
 				'check_callback': true,
 				'multiple': false,
+				'themes' : {
+					'name': 'default-dark',
+					// 'responsive' : false,
+					// 'variant' : 'small',
+					// 'stripes' : true,
+					'url': "/static/modules/jstree/themes/default-dark/style.min.css"
+				}
 			},
 			'force_text' : true,
-			'themes' : {
-				'responsive' : false,
-				'variant' : 'small',
-				'stripes' : true
-			},
 			'contextmenu' : {
 				'items' : function(node) {
 					var _tmp = $.jstree.defaults.contextmenu.items();
@@ -122,8 +99,66 @@ $(function () {
 				'file' : { 'valid_children' : [], 'icon' : 'jstree-file' },
 				'folder' : {'icon' : 'jstree-folder' },
 			},
-			'plugins' : ['state','dnd','types','contextmenu']
-		}).on('changed.jstree', function (e, data) {
+			'plugins' : ['state','dnd','types','contextmenu', 'themes']
+		}),
+		menu: $('#menu').jstree()
+	};
+	var events = {
+		docMainEvt: function () {
+			$('#docs').on('keydown', function (e) {
+				w =e;
+				console.log(e);
+				switch(e.which) {
+					case 72: // left h
+						break;
+					case 75: // up k
+						// e.preventDefault();
+						e.keyCode = 38;
+						e.which = 38;
+						e.key = 'ArrowUp';
+						$('#docs').trigger(e);
+						break;
+					case 76: // right l
+						e.preventDefault();
+						break;
+					case 74: // down j
+						e.preventDefault();
+						break;
+					case 71: // home g end G
+						e.preventDefault();
+						break;
+					default:
+						break;
+				}
+			})
+		}
+	};
+
+	var init = function (data) {
+		// events.docMainEvt();
+		page.editor.setTheme("ace/theme/twilight");
+		page.editor.session.setMode("ace/mode/rst");
+		page.editor.setKeyboardHandler("ace/keyboard/vim");
+		page.editor.commands.addCommand({
+			name: 'save',
+			bindKey: {win: "Ctrl-S", "mac": "Cmd-S"},
+			exec: save
+		});
+		page.editor.commands.addCommand({
+			name: 'quit',
+			exec: quit
+		});
+		ace.config.loadModule("ace/keyboard/vim", function(m) {
+			var VimApi = require("ace/keyboard/vim").CodeMirror.Vim;
+			VimApi.defineEx("write", "w", function(cm, input) {
+				cm.ace.execCommand("save");
+			});
+			VimApi.defineEx("quit", "q", function(cm, input) {
+				cm.ace.execCommand("quit");
+			});
+		});
+
+		page.menuElement.on('changed.jstree', function (e, data) {
 			if (data && data.selected && data.selected.length &&
 				data.node.type == 'file') {
 				$('#editor').hide();
@@ -157,7 +192,6 @@ $(function () {
 			});
 		})
 		.on('create_node.jstree', function (e, data) {
-			// console.log(data);
 			var parent = data.parent == '#' ? 0 : data.parent;
 			var pos = data.position;
 			$.ajax({
@@ -227,16 +261,6 @@ $(function () {
 				data.instance.refresh();
 			});
 		})
-		.on('copy_node.jstree', function (e, data) {
-			// $.get('?operation=copy_node', { 'id' : data.original.id, 'parent' : data.parent })
-			// 	.done(function (d) {
-			// 		//data.instance.load_node(data.parent);
-			// 		data.instance.refresh();
-			// 	})
-			// 	.fail(function () {
-			// 		data.instance.refresh();
-			// 	});
-		})
 		.on('edit.jstree', function (e, obj) {
 			// 获取doc源码
 			$.ajax({
@@ -256,7 +280,65 @@ $(function () {
 			}).fail(function (msg) {
 				alert(msg);
 			});
-		});
+		})
+		.on('keydown.jstree', '.jstree-anchor', $.proxy(function (e) {
+			var o = null;
+			switch(e.which) {
+				case 72: // left h
+					e.preventDefault();
+					if(this.is_open(e.currentTarget)) {
+						this.close_node(e.currentTarget);
+					}
+					else {
+						o = this.get_parent(e.currentTarget);
+						if(o && o.id !== $.jstree.root) { this.get_node(o, true).children('.jstree-anchor').focus(); }
+					}
+					break;
+				case 75: // up k
+					e.preventDefault();
+					o = this.get_prev_dom(e.currentTarget);
+					if(o && o.length) { o.children('.jstree-anchor').focus(); }
+					break;
+				case 76: // right l
+					e.preventDefault();
+					if(this.is_closed(e.currentTarget)) {
+						this.open_node(e.currentTarget, function (o) { this.get_node(o, true).children('.jstree-anchor').focus(); });
+					}
+					else if (this.is_open(e.currentTarget)) {
+						o = this.get_node(e.currentTarget, true).children('.jstree-children')[0];
+						if(o) { $(this._firstChild(o)).children('.jstree-anchor').focus(); }
+						else {
+							e.type = "click";
+							$(e.currentTarget).trigger(e);
+						}
+					} else {
+						e.type = "click";
+						$(e.currentTarget).trigger(e);
+					}
+					break;
+				case 74: // down j
+					e.preventDefault();
+					o = this.get_next_dom(e.currentTarget);
+					if(o && o.length) { o.children('.jstree-anchor').focus(); }
+					break;
+				case 71: // home g
+					e.preventDefault();
+					if (e.shiftKey) {
+						this.element.find('.jstree-anchor').filter(':visible').last().focus();
+					} else {
+						o = this._firstChild(this.get_container_ul()[0]);
+						if(o) { $(o).children('.jstree-anchor').filter(':visible').focus(); }
+					}
+					break;
+				default:
+					break;
+			}
+		}, page.menu))
+		.on('ready.jstree set_state.jstree', function (e, obj) {
+			page.menuElement.focus();
+			var sl = page.menu.get_selected();
+			page.menuElement.find('#'+ sl + '_anchor').focus()
+		})
 	};
 
 	var save = function (editor) {
