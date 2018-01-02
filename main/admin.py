b63f2440ed1c4eba3e145adba9b094856654a380
lulinfeng <811191000@qq.com>
2018-01-02 11:05:43 +0800

    update admin action

--- main/admin.py
+++ main/admin.py
@@ -1,14 +1,44 @@
+import json
 from django.contrib import admin
 from main.models import DocModel
+from main.views import static_doc, unstatic_doc
 
 
 def make_publish(modeladmin, reqeust, qs):
-	qs.update(status=1)
-	# make_publish.short_description = 'Mark selected doc as published'
+	root = False
+	for d in qs.filter(status=0):
+		if d.doctype | 1 == d.doctype:
+			d.doctype = (d.doctype | 8) ^ 8
+			d.status = 1
+			static_doc(d)
+			if root is False:
+				root = 0 in json.loads(d.parent)
+	# may be has childen published on up opration
+	# so not to filter status=0
+	for d in qs:
+		# static all folder
+		if d.doctype | 2 == d.doctype:
+			d.doctype = (d.doctype | 8) ^ 8
+			d.status = 1
+			static_doc(d)
+			if root is False:
+				root = 0 in json.loads(d.parent)
+	if root:
+		static_doc(type('', (object,), {'doctype': 2, 'status': 1, 'pk': 0, 'parent': '[]'}))
 
 
 def make_unpublish(modeladmin, reqeust, qs):
-	qs.update(status=0)
+	parents = set()
+	for d in qs.filter(status=1):
+		d.status = 0
+		d.doctype |= 8
+		unstatic_doc(d)
+		parents.update(json.loads(d.parent))
+
+	for p in DocModel.objects.filter(pk__in=parents):
+		static_doc(p)
+	if 0 in parents:
+		static_doc(type('', (object,), {'doctype': 2, 'status': 1, 'pk': 0, 'parent': '[]'}))
 
 
 @admin.register(DocModel)
